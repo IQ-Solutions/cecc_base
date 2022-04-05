958067c9ff6614ea6f70b716a236d7f7f5876843
krice <krice@iqsolutions.com>
2022-04-05 10:50:54 -0400

    minor error fix

--- theme-settings.php
+++ theme-settings.php
@@ -10,7 +10,7 @@ use Drupal\Core\Form\FormStateInterface;
 /**
  * Implements hook_form_system_theme_settings_alter().
  */
-function uswds_base_form_system_theme_settings_alter(&$form, FormStateInterface &$form_state, $form_id = NULL) {
+function cecc_base_form_system_theme_settings_alter(&$form, FormStateInterface &$form_state, $form_id = NULL) {
 
 
   // Menu behavior.
